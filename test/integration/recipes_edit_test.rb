bf093ac129966b71d7e2aa9cf06e276f8817c270
Dardeep Somel <dardeepsomel@gmail.com>
2017-07-25 23:46:36 +0000

    added update page

--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -17,11 +17,19 @@ class RecipesEditTest < ActionDispatch::IntegrationTest
   end
   
   test "successfully edit a recipe" do
-  
-  
-  
-
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_description = "updated description name"
+    patch recipe_path(@recipe), params: {recipe: {name: updated_name, description: updated_description }}
+    assert_redirected_to @recipe
+    #same as follow_redirect!
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_description, @recipe.description
   end
+  
   
   
   
