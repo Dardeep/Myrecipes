bc984dcde8617fa3e558d455b37cdb38ce01fa41
Dardeep Somel <dardeepsomel@gmail.com>
2017-08-02 23:42:11 +0000

    Attempt to fix DB

--- db/migrate/20170714104251_change_in_colomn_recipes.rb
+++
@@ -1,6 +0,0 @@
-class ChangeInColomnRecipes < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :recipes, :text, :description
-    change_column :recipes, :description, :text
-  end
-end
--- db/migrate/20170714104630_change_column_in_recipes.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnInRecipes < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :recipes, :text, :description
-    change_column :recipes, :description, :text
-  end
-end
